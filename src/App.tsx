c0b65990455b22d0d9a79c9c87b3cb7902851083
gouji-note <gou_corporation@yahoo.co.jp>
2017-12-07 19:24:16 +0900

    add Table Tag

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,22 @@ import * as React from 'react';
 // import * as ReactDOM from 'react-dom';
 import './App.css';
 
+/*
+<a href="https://google.com">Link</>
+table
+canvas
+<label><input type="checkbox" name="hobby" value="sports">スポーツ</label>
+<LABEL>進捗状況（ 30 ％）： <PROGRESS VALUE="30" MAX="100"></PROGRESS></LABEL>　
+<fieldset></fieldset>
+*/
+
+function makeArray(start: number, stop: number, stride: number): number[] {
+  const ary: number[] = [];
+  for (let i = start; i <= stop; i += stride) {
+    ary.push(i);
+  }
+  return ary;
+}
 
 interface ButtonProps {
   children: number[];
@@ -11,22 +27,60 @@ class Button extends React.Component<ButtonProps> {
     const children: React.ReactNode[] = [];
     for (let i = 0; i < this.props.children.length; i++) {
       const n = this.props.children[i];
-      children.push(<li><input type="button" value={"button" + n} /></li>);
+      var color = (Math.random() * 0xFFFFFF | 0).toString(16);
+      var randomColor = "#" + ("000000" + color).slice(-6);
+      children.push(<li><input type="button" value={"button" + n} style={{backgroundColor: randomColor}}/></li>);
     }
     return <ul>{children}</ul>;
   }
 }
 
+interface TableCellProps {
+  children: number[];
+}
+class TableCell extends React.Component<TableCellProps> {
+  public render(): React.ReactNode {
+    const children: React.ReactNode[] = [];
+    for (let i = 0; i < this.props.children.length; i++) {
+      const n = this.props.children[i];
+      children.push(<Button children={[n]}/>);
+    }
+    return <td>{children}</td>;
+  }
+}
+
+interface TableRowProps {
+  children: number[];
+}
+class TableRow extends React.Component<TableRowProps> {
+  public render(): React.ReactNode {
+    const children: React.ReactNode[] = [];
+    for (let i = 0; i < this.props.children.length; i++) {
+      children.push(<TableCell children={makeArray(this.props.children[0], this.props.children[0] + 9, 1)} />);
+    }
+    return <tr>{children}</tr>;
+  }
+}
+
+interface TableProps {
+  children: number[];
+}
+class Table extends React.Component<TableProps> {
+  public render(): React.ReactNode {
+    const children: React.ReactNode[] = [];
+    for (let i = 0; i < this.props.children.length; i++) {
+      const n = this.props.children[i];
+      children.push(<TableRow children={makeArray(0, n, 1)} />);
+    }
+    return <table>{children}</table>;
+  }
+}
 
 class App extends React.Component {
   public render(): React.ReactNode {
-    const ary: number[] = [];
-    for (let i = 0; i < 100; i++) {
-      ary.push(i);
-    }
     return (
       <div className="test">
-        <Button children={ary} />
+        <Table children={[7,6,5]} />
       </div>
     );
   }
