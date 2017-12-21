f83931519b101218f5d18210f173b8f95708bee7
gouji-note <gou_corporation@yahoo.co.jp>
2017-12-21 17:46:59 +0900

    ひとまず完成

--- src/App.tsx
+++ src/App.tsx
@@ -1,93 +1,109 @@
 import * as React from 'react';
 // import * as ReactDOM from 'react-dom';
 import './App.css';
-
-/*
-<a href="https://google.com">Link</>
-table
-canvas
-<label><input type="checkbox" name="hobby" value="sports">スポーツ</label>
-<LABEL>進捗状況（ 30 ％）： <PROGRESS VALUE="30" MAX="100"></PROGRESS></LABEL>　
-<fieldset></fieldset>
-*/
-
-function makeArray(start: number, stop: number, stride: number): number[] {
+/*function makeArray(start: number, stop: number, stride: number): number[] {
   const ary: number[] = [];
   for (let i = start; i <= stop; i += stride) {
     ary.push(i);
   }
   return ary;
-}
-
-interface ButtonProps {
-  children: number[];
-}
-class Button extends React.Component<ButtonProps> {
-  public render(): React.ReactNode {
-    const children: React.ReactNode[] = [];
-    for (let i = 0; i < this.props.children.length; i++) {
-      const n = this.props.children[i];
-      var color = (Math.random() * 0xFFFFFF | 0).toString(16);
-      var randomColor = "#" + ("000000" + color).slice(-6);
-      children.push(<li><input type="button" value={"button" + n} style={{backgroundColor: randomColor}}/></li>);
-    }
-    return <ul>{children}</ul>;
-  }
-}
-
-interface TableCellProps {
-  children: number[];
-}
-class TableCell extends React.Component<TableCellProps> {
-  public render(): React.ReactNode {
-    const children: React.ReactNode[] = [];
-    for (let i = 0; i < this.props.children.length; i++) {
-      const n = this.props.children[i];
-      children.push(<Button children={[n]}/>);
-    }
-    return <td>{children}</td>;
-  }
-}
-
-interface TableRowProps {
-  children: number[];
-}
-class TableRow extends React.Component<TableRowProps> {
-  public render(): React.ReactNode {
-    const children: React.ReactNode[] = [];
-    for (let i = 0; i < this.props.children.length; i++) {
-      children.push(<TableCell children={makeArray(this.props.children[0], this.props.children[0] + 9, 1)} />);
-    }
-    return <tr>{children}</tr>;
-  }
-}
-
-interface TableProps {
-  children: number[];
-}
-class Table extends React.Component<TableProps> {
-  public render(): React.ReactNode {
-    const children: React.ReactNode[] = [];
-    for (let i = 0; i < this.props.children.length; i++) {
-      const n = this.props.children[i];
-      children.push(<TableRow children={makeArray(0, n, 1)} />);
-    }
-    return <table>{children}</table>;
-  }
-}
-
+}*/
 
 interface AppProps {
-  name: string;
+  page: string;
 }
 class App extends React.Component<AppProps> {
+  public clicked() {
+  }
+
   public render(): React.ReactNode {
-    return (
-      <div className="test">
-        <h1>{this.props.name}</h1>
-        <Table children={[7,6,5]} />
-      </div>
-    );
+    if (this.props.page == "0") {
+      const node: React.ReactNode[] = [];
+      for (var i: number = 0; i < 6; i++) {
+        node.push(
+          <div className="button-box">
+            <input type="image" className="box" value={"box" + i} src={"sample" + i + ".png"} alt={"box" + i} onClick={this.clicked.bind(this)} />
+          </div>);
+      }
+      return (
+        <div className="all-container-0">{node}</div>
+      );
+    } else if (this.props.page == "1") {
+      const elemNum: number = 1000;
+      var sampleElem1: React.ReactNode[] = [];
+      for (var i: number = 0; i < elemNum; i++) {
+        sampleElem1.push(<li>{"sample channel" + i}</li>);
+      }
+      var sampleElem2: React.ReactNode[] = [];
+      for (var i: number = 0; i < elemNum; i++) {
+        sampleElem2.push(
+          <div className="balloon balloon-left">
+            <p>{"sample text." + (i * 2)}</p>
+          </div>
+        );
+        sampleElem2.push(
+          <div className="balloon balloon-right">
+            <p>{"sample text." + (i * 2 + 1)}</p>
+          </div>
+        );
+      }
+      var sampleElem3: React.ReactNode[] = [];
+      for (var i: number = 0; i < elemNum; i++) {
+        sampleElem3.push(<li>{"MemoMemoMemoMemoMemoMemoMemoMemoMemoMemo" + i}</li>);
+      }
+
+      return (
+        <div className="all-container-1">
+          <div className="left-container">
+            <div className="info-container">
+              <div className="face-image">
+                <a>image</a>
+              </div>
+            </div>
+            <div className="channel-container">
+              {sampleElem1}
+            </div>
+          </div>
+          <div className="center-container">
+            {sampleElem2}
+          </div>
+          <div className="right-container">
+            {sampleElem3}
+          </div>
+
+        </div>
+      );
+    } else if (this.props.page == "2") {
+      const elemNum: number = 1000;
+
+      var sampleElem: React.ReactNode[] = [];
+      for (var i: number = 0; i < elemNum; i++) {
+        sampleElem.push(
+          <div className="image-container">
+            <img src="sample.jpg" />
+          </div>
+        );
+      }
+
+      return (
+        <div className="all-container-2">
+          <div className="top-container">
+
+            <input type="button" value="open" />
+            <input type="button" value="rotate" />
+            <input type="button" value="flip" />
+            <input type="button" value="scale" />
+            <input type="button" value="exit" />
+          </div>
+          <div className="main-container">
+            {sampleElem}
+          </div>
+
+        </div>
+      );
+    } else {
+      return <div className="none" />;
+    }
   }
 }
 
